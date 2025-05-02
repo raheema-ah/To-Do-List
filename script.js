fc3d058b54d13ca3fcaea45547d4332332c7fa86
raheema <raheemahubballi1@gmail.com>
2025-05-02 21:02:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function addTask() {
+    const input = document.getElementById("taskInput");
+    const taskText = input.value.trim();
+    if (taskText === "") return;
+  
+    const li = document.createElement("li");
+    li.innerHTML = `
+      ${taskText}
+      <button onclick="removeTask(this)">Delete</button>
+    `;
+    document.getElementById("taskList").appendChild(li);
+    input.value = "";
+  }
+  
+  function removeTask(button) {
+    const li = button.parentElement;
+    li.remove();
+  }
+  
